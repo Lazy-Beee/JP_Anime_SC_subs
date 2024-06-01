790a941e480fddca189dd90aa0ec97763b26bb8d
Lazy-Beee <85976834+Lazy-Beee@users.noreply.github.com>
2024-06-01 03:05:42 -0400

    Add multi sub support

--- 小寄巧/font_subset_merge_v3.py
+++ 小寄巧/font_subset_merge_v3.py
@@ -1,46 +1,65 @@
 import sys, os, subprocess
 
-# mkvmerge.exe的路径，可以在mkvtoolnix安装文件夹中找到
+# mkvmerge的路径，win用户可以在mkvtoolnix安装文件夹中找到mkvmerge.exe; Linux用户安装mkvtoolnix即可
 mkvmerge_path = 'F:/Encode tools/mkvmerge.exe'
+if sys.platform == "linux" or sys.platform == "linux2":
+  mkvmerge_path = "mkvmerge"
 
-def merge_single(mkv_list, ass_list, source_dir, dump_dir):
+default_setting_single = (['0:zh-CN'], ['0:yes'], ['0:简体中文'])   # 单字幕轨默认参数
+default_setting_double = (['0:zh-CN','0:zh-TW'], ['0:yes','0:no'], ['0:简体中文','0:繁体中文'])   # 双字幕轨默认参数
+
+def get_merge_setting(ass_list, mkv_ass_ratio):
+  print('提示：存在多字幕轨时，请确保字幕按集数排序')   # 如 v1.sc1.ass, v1.sc2.ass, v1.sc3.ass, v2.sc1.ass
+  print('=====请输入字幕轨参数，用逗号分隔=====')
+  print('- 字幕语言: zh-CN（简中）, zh-TW（台繁）, ...')
+  print('- 默认轨标记: yes, no')
+  print('- 轨道名称: (自行发挥)\n')
+
+  language_list = []
+  default_track_list = []
+  track_name_list = []
+  for i in range(mkv_ass_ratio):
+    print(f'字幕轨{i+1} ({ass_list[i]})')
+    s = input('字幕语言, 默认轨标记, 轨道名称: ').replace(' ', '').split(',')
+    while len(s) != 3:
+      s = input('输入参数个数错误，请重新输入（输入q退出）: ').replace(' ', '').split(',')
+      if s[0] == 'q':
+        return
+    language_list.append(f'0:{s[0]}')
+    default_track_list.append(f'0:{s[1]}')
+    track_name_list.append(f'0:{s[2]}')
+
+  print('=====字幕轨参数收集完成=====')
+  return (language_list, default_track_list, track_name_list)
+
+def merge_mkv_ass(mkv_list, ass_list, source_dir, dump_dir, mkv_ass_ratio, setting):
+  language_list = setting[0]
+  default_track_list = setting[1]
+  track_name_list = setting[2]
+
   for i in range(len(mkv_list)):
     mkv = mkv_list[i]
     mkv_path = os.path.join(source_dir, mkv)
     mkv_path_dump = os.path.join(dump_dir, mkv)
-    ass = ass_list[i]
-    ass_path = os.path.join(source_dir, ass)
-    ass_path_dump = os.path.join(dump_dir, ass)
-    print(f'\n任务 {i+1}:\n视频：{mkv}\n字幕：{ass}')
     os.rename(mkv_path, mkv_path_dump)
-    os.rename(ass_path, ass_path_dump)
-    subprocess.run([mkvmerge_path,
-                    '--output', mkv_path,
-                    '--no-subtitles', '--no-attachments', mkv_path_dump, 
-                    '--language', '0:zh-CN', '--default-track', '0:yes', '--track-name', '0:简体中文', ass_path_dump]) # 这里设置单字幕字幕轨的参数
+    ass_path_dump_list = []
+    message = f'\n任务 {i+1}:\n视频: {mkv}'
+    command = [mkvmerge_path, '--output', mkv_path, '--no-subtitles', '--no-attachments', mkv_path_dump]
+
+    for j in range(mkv_ass_ratio):
+      ass = ass_list[i * mkv_ass_ratio + j]
+      ass_path = os.path.join(source_dir, ass)
+      ass_path_dump = os.path.join(dump_dir, ass)
+      os.rename(ass_path, ass_path_dump)
+      ass_path_dump_list.append(ass_path_dump)
+      message += f'\n字幕: {ass}'
+      command.extend(['--language', language_list[j], '--default-track', default_track_list[j], '--track-name', track_name_list[j], ass_path_dump])
+
+    print(message)
+    subprocess.run(command)
     print(f'任务 {i+1} 混流完毕')
 
-def merge_double(mkv_list, ass_list, source_dir, dump_dir):
-  for i in range(len(mkv_list)):
-    mkv = mkv_list[i]
-    mkv_path = os.path.join(source_dir, mkv)
-    mkv_path_dump = os.path.join(dump_dir, mkv)
-    ass1 = ass_list[2*i]
-    ass1_path = os.path.join(source_dir, ass1)
-    ass1_path_dump = os.path.join(dump_dir, ass1)
-    ass2 = ass_list[2*i+1]
-    ass2_path = os.path.join(source_dir, ass2)
-    ass2_path_dump = os.path.join(dump_dir, ass2)
-    print(f'\n任务 {i+1}:\n视频：{mkv}\n字幕1：{ass1}\n字幕2：{ass2}')
-    os.rename(mkv_path, mkv_path_dump)
-    os.rename(ass1_path, ass1_path_dump)
-    os.rename(ass2_path, ass2_path_dump)
-    subprocess.run([mkvmerge_path,
-                    '--output', mkv_path,
-                    '--no-subtitles', '--no-attachments', mkv_path_dump,
-                    '--language', '0:zh-CN', '--default-track', '0:yes', '--track-name', '0:简体中文', ass1_path_dump, # 这里设置双字幕字幕轨1的参数
-                    '--language', '0:zh-TW', '--default-track', '0:no', '--track-name', '0:繁体中文', ass2_path_dump]) # 这里设置双字幕字幕轨2的参数
-    print(f'任务 {i+1} 混流完毕')
+  print(f'\n{len(mkv_list)}个任务全部混流完毕！')
 
 def merge(source_dir=""):
   if source_dir == "":
@@ -62,21 +81,33 @@ def merge(source_dir=""):
   mkv_count = len(mkv_list)
   ass_count = len(ass_list)
 
-  if mkv_count == ass_count:
-    merge_single(mkv_list, ass_list, source_dir, dump_dir)
-  elif mkv_count * 2 == ass_count:
-    merge_double(mkv_list, ass_list, source_dir, dump_dir)
-  else:
-    input(f'视频数量({mkv_count})与字幕数量({ass_count})不匹配，请检查后继续 [Enter]')
+  if (mkv_count == 0) | (ass_count == 0):
+    input(f'视频数量({mkv_count})或字幕数量({ass_count})为零，请检查后继续 [Enter]')
     return
+  if (ass_count % mkv_count) != 0:
+    input(f'视频数量({mkv_count})与字幕数量({ass_count})无法整除，请检查后继续 [Enter]')
+    return
+  
+  mkv_ass_ratio = int(ass_count / mkv_count)
+  if mkv_ass_ratio == 1:
+    if input(f'使用默认参数？ [y/N]') == 'y':
+      merge_mkv_ass(mkv_list, ass_list, source_dir, dump_dir, mkv_ass_ratio, default_setting_single)
+      return
+  elif mkv_ass_ratio == 2:
+    if input(f'使用默认参数？ [y/N]') == 'y':
+      merge_mkv_ass(mkv_list, ass_list, source_dir, dump_dir, mkv_ass_ratio, default_setting_double)
+      return
     
-  print(f'\n{len(mkv_list)}个任务全部混流完毕！')
+  setting = get_merge_setting(ass_list, mkv_ass_ratio)
+  merge_mkv_ass(mkv_list, ass_list, source_dir, dump_dir, mkv_ass_ratio, setting)
 
 if __name__ == '__main__':
+  # 终端模式，循环执行直到用户手动终止
   if len(sys.argv) < 2:
     merge()
-    while (input('再来一轮? [y/N]') == 'y'):
+    while input('再来一轮? [y/N]') == 'y':
       merge()
+  # 命令行模式，依次执行参数中的目标文件夹，执行完毕后退出
   else:
     for source_dir in sys.argv[1:]:
       merge(source_dir)
